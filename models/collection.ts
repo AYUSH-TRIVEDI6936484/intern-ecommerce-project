282948946f213f82e90f436358c687cb12b18793
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2024-06-18 12:37:57 +0530

    feat: category

--- models/collection.ts
+++ models/collection.ts
@@ -1,25 +1,24 @@
 export interface Product {
-    title: string;
-    stockStatus: string;
-    slug: string;
-    type: string;
-    salesPrice: number;
-    price: number;
-    id: string;
-    featureImage: string;
-  }
-  
-  export interface Category {
-    id: string;
-    slug: string;
-    title: string;
-    featureImage: string;
-    Product: Product[];
-  }
-  
-  export interface HomePageData {
-    storePublic: {
-      collection: Category[];
-    };
-  }
-  
+  title: string;
+  stockStatus: string;
+  slug: string;
+  type: string;
+  salesPrice: number;
+  price: number;
+  id: string;
+  featureImage: string;
+}
+
+export interface Category {
+  id: string;
+  slug: string;
+  title: string;
+  featureImage: string;
+  Product: Product[];
+}
+
+export interface HomePageData {
+  storePublic: {
+    collection: Category[];
+  };
+}
